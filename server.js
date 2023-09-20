7017e8604582990ec7d84c519e5903028cd07ca3
Fuad Maulana <fuadm03@gmail.com>
2023-09-20 23:52:05 +0700

    add route

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ const fs = require('fs');
 
 const PORT = 8080;
 
-fs.readFile('./pages/index.html', (error, html) => {
-  if(error) throw error;
-  
-  http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
+  if(req.url == '/'){
+    fs.readFile('./pages/index.html', (error, html) => {
+      if(error) throw error;
+      res.writeHeader(200, {"Content-Type": "text/html"});
+      res.write(html);
+      res.end();
+    });
+  } else {
     res.writeHeader(200, {"Content-Type": "text/html"});
-    res.write(html);
+    res.write('NOT FOUND');
     res.end();
-  
-  }).listen(PORT, () => console.log(`server running on port ${PORT}`));
+  }
 });
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
 
