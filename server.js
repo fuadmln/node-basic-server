be5460d9d8b1756e928a404af547bca750a167a2
Fuad Maulana <fuadm03@gmail.com>
2023-09-20 23:45:43 +0700

    serve response from html

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 
 const PORT = 8080;
 
-http.createServer((req, res) => {
-  res.writeHeader(200, {"Content-Type": "text/html"});
-  res.write("Hello Node JS");
-  res.end();
+fs.readFile('./pages/index.html', (error, html) => {
+  if(error) throw error;
+  
+  http.createServer((req, res) => {
+    res.writeHeader(200, {"Content-Type": "text/html"});
+    res.write(html);
+    res.end();
+  
+  }).listen(PORT, () => console.log(`server running on port ${PORT}`));
+});
 
-}).listen(PORT);
